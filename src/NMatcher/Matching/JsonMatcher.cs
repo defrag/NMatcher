3fea7004cacd7dce124081162da513af1dc154a3
defrag <dabrowski@brillante.pl>
2018-10-20 12:58:42 +0200

    Small traversal readability refactor

--- src/NMatcher/Matching/JsonMatcher.cs
+++ src/NMatcher/Matching/JsonMatcher.cs
@@ -20,25 +20,23 @@ namespace NMatcher.Matching
 
         public Result Match(object value)
         {
-            var result = IterateMatch(value.ToString(), _expectedJson);
+            var result = TraverseMatch(value.ToString(), _expectedJson);
 
-            var expectedDiff = result.Item2.Except(result.Item3);
+            var expectedDiff = result.ActualPaths.Except(result.ResolvedPaths);
             if (expectedDiff.Any())
             {
                 return Result.Failure($"Expected value did not appear at path {expectedDiff.First()}.");
             }
 
-            var actualDiff = result.Item3.Except(result.Item2);
+            var actualDiff = result.ResolvedPaths.Except(result.ActualPaths);
             if (actualDiff.Any())
             {
                 return Result.Failure($"Actual value did not appear at path {actualDiff.First()}.");
             }
 
-            var error = result.Item1
-                   .Select(ToPairingResult)
-                   .FirstOrDefault(_ => false == _.Successful);
-
-            return error ?? Result.Success();
+            return result.Pairs
+                   .Select(MatchPair)
+                   .FirstOrDefault(_ => false == _.Successful) ?? Result.Success();
         }
 
         private string FormatError(object act, object exp, string path)
@@ -49,7 +47,7 @@ namespace NMatcher.Matching
             return $"{ac} did not match {ec} at path {path}.";
         }
 
-        private Result ToPairingResult(JsonPair pair)
+        private Result MatchPair(JsonPair pair)
         {
             if (pair.Expected != null && ExpressionMatcher.MatcherRegex.IsMatch(pair.Expected.ToString()))
             {
@@ -59,12 +57,12 @@ namespace NMatcher.Matching
             return pair.IsEqual ? Result.Success() : Result.Failure(FormatError(pair.Actual, pair.Expected, pair.Path));
         }
 
-        private Tuple<IEnumerable<JsonPair>, IEnumerable<string>, IEnumerable<string>> IterateMatch(string actual, string expected)
+        private TraversalResult TraverseMatch(string actual, string expected)
         {
             var actualJson = JsonTokenLoader.LoadJson(actual);
             var expectedJson = JsonTokenLoader.LoadJson(expected);
             var actualPaths = JsonTraversal.AccumulatePaths(actualJson).ToList();
-            var resolvedPath = new List<string>();
+            var resolvedPaths = new List<string>();
             var pairs = new List<JsonPair>();
 
             JsonTraversal.TraverseJson(expectedJson, token =>
@@ -98,10 +96,25 @@ namespace NMatcher.Matching
                 var comparisonResult = currentNode != null ? currentNode.Equals(expectedNode) : false;
                 pairs.Add(new JsonPair(actualValue, expectedValue, token.Path, comparisonResult));
 
-                resolvedPath.AddRange(JsonTraversal.AccumulatePaths(accumulate));
+                resolvedPaths.AddRange(JsonTraversal.AccumulatePaths(accumulate));
             });
 
-            return Tuple.Create(pairs.AsEnumerable(), actualPaths.AsEnumerable(), resolvedPath.AsEnumerable());
+            return new TraversalResult(pairs.AsEnumerable(), actualPaths.AsEnumerable(), resolvedPaths.AsEnumerable());
+        }
+
+        private sealed class TraversalResult
+        {
+            public TraversalResult(IEnumerable<JsonPair> pairs, IEnumerable<string> actualPaths, IEnumerable<string> resolvedPaths)
+            {
+                Pairs = pairs;
+                ActualPaths = actualPaths;
+                ResolvedPaths = resolvedPaths;
+            }
+
+            public IEnumerable<JsonPair> Pairs { get; }
+            public IEnumerable<string> ActualPaths { get; }
+            public IEnumerable<string> ResolvedPaths { get; }
+
         }
     }
 }
