ef84ca403458f629cc9ba8e493d02d29ec95b333
Michal Dabrowski <dabrowski@brillante.pl>
2023-05-23 14:55:59 +0200

    Fluent assertions extension project

--- build.ps1
+++ build.ps1
@@ -1,6 +1,12 @@
+$version=$args[0]
+
+if (!$version)
+{
+    Write-Output "Please provide version as first argument"
+    exit 1
+}
 
 echo "build: Version is $version"
-
 
 Push-Location $PSScriptRoot
 
@@ -14,16 +20,24 @@ Get-ChildItem -Path .\src -Filter *.csproj -Recurse | ForEach-Object {
 	if($LASTEXITCODE -ne 0) { exit 1 }
 }
 
+Write-Output "Building projects..."
+Get-ChildItem -Path .\src -Filter *.csproj -Recurse | ForEach-Object { 
+	& dotnet build $_.FullName --no-cache --no-restore -c Release /p:Version=$version
+	if($LASTEXITCODE -ne 0) { exit 1 }
+}
+
 # run tests
+Write-Output "Running tests..."
 Get-ChildItem -Path .\tests -Filter *.csproj -Recurse | ForEach-Object {
-	 & dotnet test -c Release $_.FullName
+	 & dotnet test --no-build -c Release $_.FullName
 	 if($LASTEXITCODE -ne 0) { exit 1 }
 }
 
+Write-Output "Packaging files..."
 # run pack on all *.csproj files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
 Get-ChildItem -Path .\src -Filter *.csproj -Recurse | ForEach-Object {
 
-	& dotnet pack $_.FullName -c Release -o .\artifacts
+	& dotnet pack --no-build $_.FullName -c Release -o .\artifacts /p:PackageVersion=$version
 	
 	if($LASTEXITCODE -ne 0) { exit 1 }
 }
--- tests/NMatcher.IntegrationTests/JsonTests.cs
+++ tests/NMatcher.IntegrationTests/JsonTests.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using NMatcher.IntegrationTests.XUnit;
-using NMatcher.Matching.Json;
+﻿using NMatcher.IntegrationTests.XUnit;
 using Xunit;
 
 namespace NMatcher.IntegrationTests
