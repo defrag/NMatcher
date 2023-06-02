7b5e13b2042671c272b6c57bb152ac3aaea48105
Michal Dabrowski <dabrowski@brillante.pl>
2023-06-02 07:00:56 +0200

    Adding samples section with asp.net integration test

---
+++ samples/NMatcher.Samples.Api.Tests/Integration/WeatherForecastControllerTests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using NMatcher.FluentAssertions;
+
+namespace NMatcher.Samples.Api.Tests.Integration;
+
+public class WeatherForecastControllerTests
+{
+    [Fact]
+    public async Task test_weather_forecast_endpoint()
+    {
+        var program = new TestApiProgram();
+        var client = program.CreateClient();
+        
+        var rs = await client.GetAsync("/WeatherForecast");
+        var response = await rs.Content.ReadAsStringAsync();
+
+        response.Should().MatchJson(@"
+        [
+          {
+            ""date"": ""@string@.IsDateTime()"",
+            ""temperatureC"": ""@int@"",
+            ""temperatureF"": ""@int@"",
+            ""summary"": ""@string@.OneOf('Freezing', 'Cool', 'Mild', 'Warm', 'Balmy', 'Hot')""
+          },
+          ""@skip@""
+        ]
+        ");
+    }
+}
---
+++ samples/NMatcher.Samples.Api.Tests/TestApiProgram.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace NMatcher.Samples.Api.Tests;
+
+sealed class TestApiProgram : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test");
+    }
+}
